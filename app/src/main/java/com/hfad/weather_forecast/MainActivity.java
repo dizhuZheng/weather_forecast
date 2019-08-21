39d67b1124f6bc3f77adfae6589fcffffbc20d02
dizhu <dizhu210@gmail.com>
2019-08-20 21:58:44 -0400

    welcome page can wait 3 seconds to the second one

---
+++ app/src/main/java/com/hfad/weather_forecast/MainActivity.java
@@ -0,0 +1,25 @@
+package com.hfad.weather_forecast;
+import android.os.Handler;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.os.Bundle;
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+    private final int SPLASH_DISPLAY_LENGTH = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, ShowInfoActivity.class);
+                MainActivity.this.startActivity(intent);
+                MainActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
---
+++ app/src/main/java/com/hfad/weather_forecast/ShowInfoActivity.java
@@ -0,0 +1,9 @@
+package com.hfad.weather_forecast;
+import android.os.Bundle; import android.app.Activity;
+public class ShowInfoActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_show_info);
+    }
+}
